1025b683d96a40c46935e5c7832541c20c359b1c
Rohan Kapur <me@rohankapur.com>
2015-08-05 22:09:00 +0800

    Completed ex. 5

---
+++ 4_largestpalindrome.py
@@ -0,0 +1,19 @@
+def getPalindromeFromMaxDigits(digits):
+	v1 = ((10 ** digits) - 1)
+	v2 = v1
+	shift = 10 ** (digits - 1)
+	palindrome = 0
+	for i in xrange(v1 - shift):
+		for j in xrange(v2 - shift):
+			p = (v1-i)*(v2-j)
+			palindromeStr = str(p)
+			length = len(palindromeStr)
+			if length and (not length % 2) and palindromeStr[:length/2] == palindromeStr[length/2:][::-1]:
+				if p > palindrome:
+					palindrome = p
+					print "Found higher palindrome: " + str(palindrome)
+	return palindrome
+
+print "Input number of max. digits:"
+digits = int(raw_input())
+print "Highest palindrome found: " + str(getPalindromeFromMaxDigits(digits)) # Inefficient for > 3 characters
